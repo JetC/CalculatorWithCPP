8161b3cca2b42b3378a504ada3b51ed5ad973737
Sun PeiFeng <g7733pf@gmail.com>
2014-09-24 09:45:42 +0800

    Initial Commit

---
+++ CalculatorWithCPP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CalculatorWithCPP
+//
+//  Created by 孙培峰 on 9/24/14.
+//  Copyright (c) 2014 孙培峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CalculatorWithCPP/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CalculatorWithCPP
+//
+//  Created by 孙培峰 on 9/24/14.
+//  Copyright (c) 2014 孙培峰. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
