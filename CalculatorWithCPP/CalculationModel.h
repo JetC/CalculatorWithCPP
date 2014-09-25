39b1300125f91b04c67420b76d921c0856ee26c1
Sun PeiFeng <g7733pf@gmail.com>
2014-09-25 22:19:51 +0800

    [dev]实现拆分数字

--- CalculatorWithCPP/CalculationModel.h
+++ CalculatorWithCPP/CalculationModel.h
@@ -8,7 +8,7 @@
 
 #import <Foundation/Foundation.h>
 
-#include <stdio.h>
+#include <iostream>
 //@interface CalculationModel : NSObject
 
 class CalculationModel{
@@ -24,9 +24,50 @@ public:
         long Result = 0;
         //打印输入内容
         printf("This time inputed into CPP:\n");
+        long Numbers[100];
+        char Operators[20];
+        char SingleNumber[10];
+        int SingleNumberIndex = 0;
+        int NumbersIndex = 0;
+        int OperatorsIndex = 0;
         for (int i = 0; i < CharactersCountInExpression; i++) {
             
-            printf("%d",isNumber(Expression[i]));
+            //分析单个字符
+            if (isNumber(Expression[i]) && i != CharactersCountInExpression-1) {
+                
+                SingleNumber[SingleNumberIndex] = Expression[i];
+                SingleNumberIndex++;
+            }
+            else if (isExpression(Expression[i]) && i > 0 && i != CharactersCountInExpression-1){
+                
+                //SingleNumber转为long的数字
+                Numbers[NumbersIndex] = strtol(SingleNumber, NULL, 10);
+                printf("Number:%ld\n",Numbers[NumbersIndex]);
+                NumbersIndex++;
+                
+                //复位SingleNumber
+                memset(SingleNumber, 0,strlen(SingleNumber));
+                SingleNumberIndex = 0;
+                
+                Operators[OperatorsIndex] = Expression[i];
+                OperatorsIndex++;
+            }
+            else if (i == CharactersCountInExpression-1 && isNumber(Expression[i])){
+                
+                SingleNumber[SingleNumberIndex] = Expression[i];
+                SingleNumberIndex++;
+                
+                Numbers[NumbersIndex] = strtol(SingleNumber, NULL, 10);
+                printf("Number:%ld\n",Numbers[NumbersIndex]);
+            }
+            else{
+                printf("Error");
+                abort();
+            }
+            
+            
+            
+            
         }
         printf("\n");
         
@@ -51,6 +92,31 @@ public:
             return NO;
         }
     };
+    
+    BOOL isExpression(const char character){
+        
+        if (character == '+' || character == '-' || character == '*' || character == '/') {
+            return YES;
+        }
+        else{
+            return NO;
+        }
+    };
+    
+    char makeSingleToNumber(const char character[], int characterCount){
+        
+        char Number[100];
+        if (characterCount >= 100) {
+            printf("Error");
+            abort();
+        }
+        for (int i = 0; i < characterCount ; i++) {
+            std::cout<<"ds";
+            Number[i] = character[i];
+        }
+        
+        return 0;
+    }
     
     
     
